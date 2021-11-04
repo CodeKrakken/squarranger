ce7821752e2ef7f7e6b2596328db6e1a853422a5
CodeKrakken <newholm1@hotmail.com>
2021-11-04 18:18:36 +0000

    Switch

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -2,7 +2,9 @@ const Checkbox = (data) => {
   console.log(data)
 
   return (
-    <input type="checkbox" defaultChecked={data.checked} />
+    <input type="checkbox" defaultChecked={data.checked} onChange={e => {
+      setBox(e.target.checked);
+    }} />
   );
 }
  
