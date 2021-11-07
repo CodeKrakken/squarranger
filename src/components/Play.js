b66af6da837028fa8eff751551ea4c720fb19197
CodeKrakken <newholm1@hotmail.com>
2021-11-07 11:14:51 +0000

    Iterate over beats array with timeouts

--- src/components/Play.js
+++ src/components/Play.js
@@ -5,8 +5,23 @@ const Play = () => {
 
   const context = useContext(Context)
 
+  var a = [
+    1,2,3,4,5,6,7,8,9,10
+    ];
+
+  const play = (beats, i=0) => {
+    if (beats[i]) { console.log('BANG!') }
+    if (i<beats.length){
+       setTimeout(function(){
+           i++;
+           play(beats, i);
+       },1000);
+    }
+  }
+
   const handleClick = (e) => {
-    console.log(e)
+    console.log('Playing')
+    play(context.state.boxes)
   }
 
   return ( 
